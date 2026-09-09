8769c5d08fa2872954561778b55726306622a0f5
JaquelineDisktrans <jaqueline@disktrans.com.br>
2026-09-09 14:19:15 -0300

    Clean up data.js by removing unused exports
    Removed commented-out code and export statements for series and current ranking data.

--- js/data.js
+++ js/data.js
@@ -57,32 +57,3 @@ export const comparativo = {
     { nome: 'PIAUI EMP',        hb13: 1,  hm3: 0,  ob13: 0, om3: 0,   ontem: null, hoje: 1 }
   ]
 };
-// Dados consolidados do FUP Prioridades (P4 - somente CSS).
-// Fonte: exports "priorizacao_trocas" tratados manualmente.
-// Faixas de atraso derivadas de Dias_atraso: 1 a 3 dias e mais de 3 dias.
-// Nenhum dado pessoal - apenas nomes de concessionarias e contagens agregadas.
-
-export const serie = [
-  { data: '02/09', rotulo: '02/09', conc: 14, sol: 106, equip: 353, b13: 156, m3: 197 },
-  { data: '04/09', rotulo: '04/09', conc: 14, sol: 99,  equip: 327, b13: 145, m3: 182 },
-  { data: '08/09', rotulo: '08/09', conc: 13, sol: 88,  equip: 280, b13: 0,   m3: 280 },
-  { data: '09/09', rotulo: '09/09', conc: 15, sol: 73,  equip: 241, b13: 62,  m3: 179 }
-];
-
-export const rankingAtual = [
-  { nome: 'NIGRO',            b13: 24, m3: 58, equip: 82, sol: 32 },
-  { nome: 'KARIRI EMP.',      b13: 10, m3: 50, equip: 60, sol: 10 },
-  { nome: 'FORKMAX',          b13: 16, m3: 43, equip: 59, sol: 10 },
-  { nome: 'EMPILHASERV',      b13: 6,  m3: 6,  equip: 12, sol: 3 },
-  { nome: 'EMPILHAPARA',      b13: 4,  m3: 2,  equip: 6,  sol: 3 },
-  { nome: 'EMPILOG CASCAVEL', b13: 0,  m3: 6,  equip: 6,  sol: 3 },
-  { nome: 'JR HIDRAULICOS',   b13: 0,  m3: 4,  equip: 4,  sol: 3 },
-  { nome: 'BAIANA EQUIP.',    b13: 0,  m3: 3,  equip: 3,  sol: 2 },
-  { nome: 'BHM',              b13: 0,  m3: 2,  equip: 2,  sol: 1 },
-  { nome: 'NIGRO MANAUS',     b13: 0,  m3: 2,  equip: 2,  sol: 1 },
-  { nome: 'AGRS',             b13: 1,  m3: 0,  equip: 1,  sol: 1 },
-  { nome: 'BAIANA VDC',       b13: 0,  m3: 1,  equip: 1,  sol: 1 },
-  { nome: 'EMPILOG CHAPECO',  b13: 0,  m3: 1,  equip: 1,  sol: 1 },
-  { nome: 'PIAUI EMP',        b13: 1,  m3: 0,  equip: 1,  sol: 1 },
-  { nome: 'STAMM',            b13: 0,  m3: 1,  equip: 1,  sol: 1 }
-];
